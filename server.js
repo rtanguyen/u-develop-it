fd73da3d206bfc17f1ef794bdc6a44fc6a496a32
rtanguyen <rtanguyen@gmail.com>
2021-06-04 19:58:32 -0500

    add voter routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mysql = require('mysql2');
-const inputCheck = require('./utils/inputCheck');
+const db = require('./db/connection')
+const apiRoutes = require('./routes/apiRoutes')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,100 +8,20 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-//connect to database
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: 'asdfghjkl2!',
-        database: 'election'
-    },
-    console.log('Connected to the election database.')
-);
 
-//get all candidates
-app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates` ;
-
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({error: err.message});
-            return;
-        }    
-            res.json({
-                message: 'success',
-                data: rows
-        });
-    });
-});
-
-//GET a single candidate
-app.get('/api/candidates/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
-
-    db.query(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({error: err.message});
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
-
-// //DELETE a candidate
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id]
-   
-    db.query(sql, params, (err, results) => {
-        if (err) {
-            res.statusMessage(400).json({error: res.message});
-        } else if (!results.affectedRows) {
-            res.json({
-                message: 'Candidate not found'
-            });
-        } else {
-            res.json({
-                message: 'deleted',
-                changes: results.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
-
-//Create a candidate
-app.post('/api/candidate', ({body}, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-    if (errors) {
-        res.status(400).json({error: errors});
-        return;
-    }
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-            VALUES (?, ?, ?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({error: errors});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
-    });
-});
+//use apiRoutes
+app.use('/api', apiRoutes);
 
 //default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+//start server after db connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
